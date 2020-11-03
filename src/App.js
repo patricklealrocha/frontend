7dc59ceb66ffca5a34fd98a9047c370e55cd46ee
Patrick Rocha <pdiasrocha@yahoo.com>
2020-11-02 21:39:22 -0300

    importado css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import './App.css';
 
 import Header from './components/Header';
 
+
+
 function App(){
-    return <Header />;
-}
+    const [projects, setProjects] = useState(['Desenvolvimento de app','Front-end web','ReactJS']);
+
+    function handleAddProject(){
+        //projects.push(`New Project ${Date.now()}`);
+        setProjects([...projects,`New Project ${Date.now()}`]);
+
+    }
+    return(
+        <>
+        <Header title = 'Projects'/>
+          <ul>
+              {projects.map(project => <li key = {project}>{project}</li>)}
+          </ul>
+
+          <button type="button" onClick = {handleAddProject}>Adicionar Projeto</button>
+        
+        </>
+    );
+} 
+    
 
 export default App;
